1a719b1628ba2bf995c7ff81c013fdfc70713eda
awy76 <106824144+awy76@users.noreply.github.com>
2022-10-19 10:46:09 +0300

    edit

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-//Install express server
-const express = require("express");
-const path = require("path");
+let express = require("express");
 
-const app = express();
+let app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/<name-of-app>"));
+app.use(express.static(__dirname + "/dist/stable-diffusion"));
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/<name-of-app>/index.html"));
+app.get("/*", (req, res) => {
+  res.sendFile(__dirname + "/dist/stable-diffusion/index.html");
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
